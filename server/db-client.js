d2ce41878aa3ce09bd4394a8772959dfd6e945c1
unknown <kevin@vivalink.com>
2022-08-22 18:29:11 -0700

    Ssl true

--- server/db-client.js
+++ server/db-client.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const { Client } = require('pg')
-const client = new Client(process.env.DB_CONNECTION_STRING)
+const client = new Client(process.env.DB_CONNECTION_STRING, {ssl:true})
 client.connect()
+
 
 
 module.exports = client;
