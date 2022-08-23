85a133cf2bf7816b937eef9683ab6e352c5a9a5f
unknown <kevin@vivalink.com>
2022-08-22 18:34:30 -0700

    pg client

--- server/db-client.js
+++ server/db-client.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
-const { Client } = require('pg')
-const client = new Client(process.env.DB_CONNECTION_STRING, {ssl:true})
+const pg = require('pg')
+const client = new pg.Client(process.env.DB_CONNECTION_STRING)
+pg.defaults.ssl = true;
 client.connect()
 
 
