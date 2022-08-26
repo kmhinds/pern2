448f8f3dd76ac134b807c153dd6c6b1eef9c8da9
unknown <kevin@vivalink.com>
2022-08-26 13:38:02 -0700

    Add middleware

--- server/api/index.js
+++ server/api/index.js
@@ -1,18 +1,33 @@
 const app = require('express')();
 const client = require('../db-client')
 
+// function checkUser(req, res, next) {
+//   if ( req.path == '/') return next();
+
+//   //authenticate user
+//   next();
+//  }
+
+function addHeader(req, res, next) {
+  if (req.path == '/') {
+    return next();
+  }
+  next();
+}
+// app.use([(req, res) => {
+//   response.setHeader('access-control-allow-origin', '*');
+// }])
 
 app.get('/api/todos', (req, response) => {
     client.query('SELECT * FROM to_do;', (err, res) => {
     if (err) console.log(err);
-    response.setHeader('access-control-allow-origin', '*');
     response.send(res.rows);
   })
 });
 
 // TODO: Add a POST route
 // TODO: Add a DELETE route
-// TODO: ADD a PUT route
-// TODO
+// TODO: Add a PUT route
+// TODO: Add route /api/todo?
 
 module.exports = app;
