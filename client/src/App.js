b354a246b272890f99f0d6d0ca5888a82ec81d37
unknown <kevin@vivalink.com>
2022-08-26 13:12:26 -0700

    Add data to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState();
+  
+  useEffect(() => {
+    fetch(process.env.REACT_APP_SERVER_ENDPOINT).then( async (res) => {
+      const data = await res.json();
+      console.log(data);
+      setData(data);
+      setLoading(false);
+    })
+  }, [])
+
+  if (loading) {
+    return <div>loading...</div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {JSON.stringify(data)}
     </div>
   );
 }
--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,7 @@ const client = require('../db-client')
 app.get('/api', (req, response) => {
     client.query('SELECT * FROM to_do;', (err, res) => {
     if (err) console.log(err);
+    response.setHeader('access-control-allow-origin', '*');
     response.send(res.rows);
   })
   
