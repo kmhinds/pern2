7846f671c7005fc1bc79b133128f1ff393863478
unknown <kevin@vivalink.com>
2022-08-26 17:01:24 -0700

    Add POST route

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState();
+  const [description, setDescription] = useState('');
+  const [priority, setPriority] = useState('low');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(
+      process.env.REACT_APP_SERVER_ENDPOINT + '/todos',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          priority: priority,
+          description: description
+        })
+      }
+    ).then((res) => {
+      console.log(res)
+    }).catch((err) => {
+        console.error(err)
+      }
+    )
+    console.log('submit')
+  }
 
   useEffect(() => {
     fetch(process.env.REACT_APP_SERVER_ENDPOINT + '/todos').then( async (res) => {
       const data = await res.json();
-      console.log(data);
       setData(data);
       setLoading(false);
     })
@@ -21,8 +45,36 @@ function App() {
 
   return (
     <div>
+      <h1>
+        todos:
+      </h1>
+        <ul>
+          {data.map((todo)=>{
+            return (
+              <div key={todo.id}>
+                <li>Description: {todo.description} Priority: {todo.priority}</li>
+              </div>
+              
+            )
+          })}
+        </ul>
       {JSON.stringify(data)}
+      <form>
+          <label htmlFor='description' >Description</label>
+          <input name='description' id='description' type='text' value={description} onChange={(e)=> setDescription(e.target.value)} required></input>
+          <label htmlFor='priority' >Priority</label>
+          <select name='priority' id='priority' value={priority} onChange={(e)=> {
+            console.log(e.target.value,)
+            setPriority(e.target.value)
+          }} required>
+            <option value='low'>Low</option>
+            <option value='mid'>Mid</option>
+            <option value='high'>High</option>
+          </select>
+          <button onClick={handleSubmit}>Submit</button>
+      </form>
     </div>
+    
   );
 }
 
--- server/api/index.js
+++ server/api/index.js
@@ -1,16 +1,43 @@
 const app = require('express')();
 const client = require('../db-client')
+const bodyParser = require('body-parser')
 
-app.use((_, res, next) => {
+const jsonParser = bodyParser.json();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const allowOrigin = (_, res, next) => {
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('access-control-allow-origin', '*');
   next();
-})
+};
+
+
+
+app.use(allowOrigin, jsonParser)
 
 app.get('/api/todos', (req, response) => {
-    client.query('SELECT * FROM to_do;', (err, res) => {
+    client.query('SELECT * FROM to_do;', (err, dbRes) => {  
     if (err) console.log(err);
-    response.send(res.rows);
+    response.json(dbRes.rows);
   })
+});
+
+app.post('/api/todos', (req, response) => {
+  const body = req.body;
+
+  console.log(body);
+  client.query(`
+    INSERT INTO to_do (id, description, priority) 
+    VALUES (DEFAULT, '${body.description}', '${body.priority}');`
+    ), 
+    (err, dbRes) => {
+      if (err) {
+        console.log(err);
+        response.status(500).json({error: 'failed to add todo'})
+      } 
+      response.json(dbRes.rows)
+    }
 });
 
 // TODO: Add a POST route
